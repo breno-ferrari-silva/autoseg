f896af71e51de40a531d1a16aa3692da960e9c20
Breno Ferrari <brenoferrari.0208@gmail.com>
2025-01-11 22:03:07 -0300

    Arquivos com granularidade por CEP salvos no formato .csv

--- autoseg/dados.py
+++ autoseg/dados.py
@@ -1,2 +1,46 @@
+import os
+import shutil
 import pandas as pd
+from pathlib import Path
 
+from autoseg import utils
+
+def processar_arquivo_bruto_autoseg() -> None:
+    """
+    Processa o arquivo bruto da Autoseg, criando uma coluna de referência baseada
+    nos valores da coluna 'ENVIO' e exporta o resultado para um arquivo CSV processado.
+
+    O arquivo bruto é lido do diretório 'interim/autoseg', e o arquivo processado é salvo
+    no diretório 'processed/autoseg'. A referência é gerada substituindo as letras 'A' por '1'
+    e 'B' por '2' na coluna 'ENVIO'.
+
+    Args:
+        None
+
+    Returns:
+        None
+    """
+    # Diretório provisório
+    path_interim = Path('..') / 'data' / 'interim' / 'autoseg'
+
+    # Leitura do arquivo
+    df = pd.read_csv(
+        path_interim / 'arq_casco3_comp.csv',
+        sep=';',
+        encoding='ansi'
+    )
+
+    # Criando uma variável de referência
+    ref = df['ENVIO'].str.replace('A', '1')
+    ref = ref.str.replace('B', '2')
+
+    # Inserindo a referência no dataframe
+    df.insert(0, 'REF', ref)
+
+    # Exportando arquivo
+    path_save = Path('..') / 'data' / 'processed' / 'autoseg'
+    nome_save = f"{df['REF'].unique()[0]}.csv"
+    df.to_csv(path_save / nome_save, index=False)
+
+    print(f"Arquivo arq_casco3_comp da referência '{nome_save}' salvo com sucesso em '{path_save / nome_save}'.")
+    
--- autoseg/utils.py
+++ autoseg/utils.py
@@ -1,6 +1,35 @@
 import zipfile
+import os
+import shutil
 import pandas as pd
 
-def descompacta_zip(dir_zip, dir_ext):
-    with zipfile.ZipFile(dir_zip, 'r') as zip_ref:
-        zip_ref.extractall(dir_ext)
+from typing import Optional
+
+def descompactar_zip(path_zip: str, path_ext: str) -> None:
+    """
+    Descompacta um arquivo ZIP para um diretório especificado.
+
+    Args:
+        path_zip (str): Caminho para o arquivo ZIP a ser descompactado.
+        path_ext (str): Caminho para o diretório onde os arquivos serão extraídos.
+
+    Returns:
+        None
+    """
+    with zipfile.ZipFile(path_zip, 'r') as zip_ref:
+        zip_ref.extractall(path_ext)
+        print(f"Arquivo '{path_zip}' descompactado com sucesso em '{path_ext}'.")
+
+
+def limpar_pasta(path: str) -> None:
+    """
+    Remove todos os arquivos e subpastas de um diretório, deixando-o vazio.
+
+    Args:
+        path (str): Caminho para a pasta que será esvaziada.
+
+    Returns:
+        None: Não retorna nenhum valor.
+    """
+    shutil.rmtree(path)
+    os.makedirs(path)
